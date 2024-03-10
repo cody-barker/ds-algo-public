1b3a45c3055ddee70f41052ca341e6ee98068e4a
Cody <codybarker.or@gmail.com>
2024-03-10 11:34:07 -0700

    add ruby version

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,18 +1,26 @@
 def reverse_string(str)
-  # type your code in here
+  reversed = ""
+  i = str.length - 1
+  while i >= 0 
+    reversed += str[i]
+    i = i - 1
+  end
+  p reversed == str
 end
 
-if __FILE__ == $PROGRAM_NAME
-  puts "Expecting: 'ih'"
-  puts "=>", reverse_string('hi')
+reverse_string("racecar")
 
-  puts
+# if __FILE__ == $PROGRAM_NAME
+#   puts "Expecting: 'ih'"
+#   puts "=>", reverse_string('hi')
 
-  puts "Expecting: 'ybabtac'"
-  puts "=>", reverse_string('catbaby')
+#   puts
 
-  # Don't forget to add your own!
-end
+#   puts "Expecting: 'ybabtac'"
+#   puts "=>", reverse_string('catbaby')
+
+#   # Don't forget to add your own!
+# end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
