cfa4e7f3c25907da98c1c4a92c30bd6011e1b0ee
Cody <codybarker.or@gmail.com>
2024-04-01 14:34:37 -0700

    add solution 2

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -8,10 +8,11 @@
 
 function fibonacci(n) {
   if (n < 2) {
-    return n
+    return n;
   }
 
-  return fibonacci(n - 1) + fibonacci(n - 2)
+  return fibonacci(n - 1) + fibonacci(n - 2);
+
 }
 
 if (require.main === module) {
