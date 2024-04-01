27de1590391b99a60a19d979b78684625e70710a
Cody Barker <codybarker.or@gmail.com>
2024-04-01 12:05:50 -0700

    solve and add benchmark

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/javascript/find_shortest_string.js
@@ -1,6 +1,17 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortest = arr[0];
+  for (element of arr) {
+    if (element.length < shortest.length) {
+      shortest = element
+    }
+  }
+  return shortest
 }
+
+//look at each string and store its length
+//at the end, return the string with the shortest length
+//could do this by storing them in an object
+//key is i, value is length
 
 if (require.main === module) {
   // add your own tests in here
@@ -18,6 +29,29 @@ if (require.main === module) {
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
   // BENCHMARK HERE
+  const longInput = [];
+  const shortInput = [];
+  const startTime = Date.now();
+
+  for (let i = 0; i < 100; i++) {
+    longInput.push(Math.random());
+  };
+
+  for (let i = 0; i < 3; i++) {
+    shortInput.push(Math.random());
+  };
+
+  for (let i = 0; i < 1000; i++) {
+    findShortestString(longInput);
+    findShortestString(shortInput)
+  }
+
+  const endTime = Date.now();
+
+  const totalTime = endTime - startTime;
+  const averageRuntime = totalTime / 1000;
+  console.log("Average Runtime:", averageRuntime, "milliseconds")
+
 }
 
 module.exports = findShortestString;
