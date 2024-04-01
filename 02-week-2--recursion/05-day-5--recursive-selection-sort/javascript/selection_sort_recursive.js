b95933d63f9aeac02fda43cd0fbd38f9d987b5e1
Cody Barker <codybarker.or@gmail.com>
2024-04-01 13:33:28 -0700

    add solution

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,29 @@
+// function selectionSortRecursive(arr) {
+//   if (arr.length === 0) {
+//     return [];
+//   }
+
+//   const min = Math.min(...arr);
+//   const idx = arr.indexOf(min);
+//   arr.splice(idx, 1);
+
+//   const result = selectionSortRecursive(arr);
+//   result.unshift(min);
+//   return result;
+// }
+
 function selectionSortRecursive(arr) {
-  // type your code here
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min)
+  arr.splice(idx, 1)
+
+  const result = selectionSortRecursive(arr)
+  result.unshift(min)
+  return result
 }
 
 if (require.main === module) {
