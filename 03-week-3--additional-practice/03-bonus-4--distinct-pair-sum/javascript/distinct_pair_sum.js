d9d8607b25685cefd4f69f8dc41f01d0c4be27a2
Cody <codybarker.or@gmail.com>
2024-04-12 12:58:40 -0700

    add sorted solution

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -1,5 +1,33 @@
+//Return an array of distinct 2 number arrays (check for reverse or by sorting)
+//Iterate through, testing whether num1 + num2 = target,
+//   if not, num1 = num2, num2 + 1 (how to do this?)
+
+// function distinctPairSum(arr, k) {
+//   let distinctArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] + arr[i + 1] === k) {
+//       distinctArr.push(new Array(arr[i], arr[i + 1]));
+//     }
+//   }
+//   return distinctArr
+// }
+
 function distinctPairSum(arr, k) {
-  // type your code here
+  let distinctArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === k) {
+        let pair = [arr[i], arr[j]].sort((a, b) => a - b); // Sort the pair
+        if (!distinctArr.some(([x, y]) => x === pair[0] && y === pair[1])) {
+          distinctArr.push(pair);
+        }
+      }
+    }
+  }
+
+  return distinctArr;
 }
 
 if (require.main === module) {
