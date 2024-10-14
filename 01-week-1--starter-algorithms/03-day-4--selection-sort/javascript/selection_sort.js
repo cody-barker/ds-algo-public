def6a28b4844bda683476d4967d8e739763bd6d2
Cody <codybarker.or@gmail.com>
2024-10-14 15:56:06 -0700

    add nested for loop solution with array destructuring

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -3,7 +3,26 @@
 // }
 
 function selectionSort(arr) {
-  return arr.sort((a, b) => a - b);
+  // Loop through the entire array
+  for (let i = 0; i < arr.length; i++) {
+    // Assume the current index is the minimum
+    let minIndex = i;
+
+    // Check the rest of the array for a smaller element
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        // If a smaller element is found, update the minimum index
+        minIndex = j;
+      }
+    }
+
+    // If the minimum index has changed, swap the elements
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr; // Return the sorted array
 }
 
 if (require.main === module) {
