3a431d08dbd8f72e524aada8847c56f9789df809
Cody <codybarker.or@gmail.com>
2024-10-15 10:11:12 -0700

    add balancing parenthesis solution

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/javascript/balancing_parentheses.js
@@ -3,36 +3,68 @@
 //create an object and store the number of open or closed parentheses
 //subtract the difference and return that number as the number of closed needed
 
+// function balancingParentheses(string) {
+//   let store = { open: 0, closed: 0 };
+//   let arr = string.split("");
+//   for (let p of arr) {
+//     if (p === "(") {
+//       store.open += 1;
+//     } else {
+//       store.closed += 1;
+//     }
+//   }
+//   return store.closed - store.open;
+// }
+
+// balancingParentheses("(())")
+
+// function balancingParentheses(string) {
+//   let hash = { open: 0, closed: 0 };
+//   for (let i = 0; i < string.length; i++) {
+//     string[i] === "(" ? (hash.open += 1) : (hash.closed += 1);
+//   }
+
+//   return hash.closed - hash.open;
+// }
+
 function balancingParentheses(string) {
-  let store = {open: 0, closed: 0};
-  let arr = string.split("")
-  for (let p of arr) {
-    if (p === "(") {
-      store.open += 1
-    }
-    else {
-      store.closed += 1
+  let openNeeded = 0; // Keeps track of open parentheses needed
+  let closeNeeded = 0; // Keeps track of close parentheses needed
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") {
+      openNeeded += 1;
+    } else if (string[i] === ")") {
+      if (openNeeded > 0) {
+        // We have a matching open parenthesis, so reduce openNeeded
+        openNeeded -= 1;
+      } else {
+        // No open parenthesis to match, we need an extra open parenthesis
+        closeNeeded += 1;
+      }
     }
   }
-  return store.closed - store.open
+
+  // Total adjustments needed is the sum of unmatched opens and unmatched closes
+  return openNeeded + closeNeeded;
 }
 
-balancingParentheses("(())")
+console.log(balancingParentheses("(()"));
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
-  console.log(balancingParentheses('(()())'));
+  console.log(balancingParentheses("(()())"));
 
   console.log("");
 
   console.log("Expecting: 2");
-  console.log(balancingParentheses('()))'));
+  console.log(balancingParentheses("()))"));
 
   console.log("");
 
   console.log("Expecting: 1");
-  console.log(balancingParentheses(')'));
+  console.log(balancingParentheses(")"));
 }
 
 module.exports = balancingParentheses;
