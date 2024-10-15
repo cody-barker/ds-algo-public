dea84b387d7878ce68aeba301553bcbe931e3c3f
Cody <codybarker.or@gmail.com>
2024-10-15 11:38:23 -0700

    add succesful attempt at romans

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -47,38 +47,53 @@ function romanNumeral(string) {
     CD: 400,
     D: 500,
     CM: 900,
-    M: 1000
+    M: 1000,
   };
-  idx = 0;
-  total= 0;
+  // idx = 0;
+  // total = 0;
 
-  while (idx < string.length) {
-    const twoChar = string[idx] + (string[idx + 1] || '');
-    if (romans[twoChar] !== undefined) {
-      total += romans[twoChar]
-      idx += 2
+  // while (idx < string.length) {
+  //   const twoChar = string[idx] + (string[idx + 1] || "");
+  //   if (romans[twoChar]) {
+  //     total += romans[twoChar];
+  //     idx += 2;
+  //   } else {
+  //     total += romans[string[idx]];
+  //     ++idx;
+  //   }
+  // }
+  // return total;
+
+  let total = 0;
+  let index = 0;
+
+  while (index < string.length) {
+    let twoChar = string[index] + (string[index + 1] || "");
+    if (romans[twoChar]) {
+      total += romans[twoChar];
+      index += 2;
     } else {
-      total += romans[string[idx]]
-      ++idx
+      total += romans[string[index]];
+      ++index;
     }
   }
-  return total
+  return total;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 1");
-  console.log(romanNumeral('I'));
+  console.log(romanNumeral("I"));
 
   console.log("");
 
   console.log("Expecting: 9");
-  console.log(romanNumeral('IX'));
+  console.log(romanNumeral("IX"));
 
   console.log("");
 
   console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
+  console.log(romanNumeral("CDII"));
 }
 
 module.exports = romanNumeral;
