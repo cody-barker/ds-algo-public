cf177301c6900cf26a068659608f379726a6f7ab
Cody <codybarker.or@gmail.com>
2024-10-17 14:27:37 -0700

    add code review

--- 04-pairing-exercises-1/00-code-review/code-review.js
+++ 04-pairing-exercises-1/00-code-review/code-review.js
@@ -4,10 +4,9 @@ function printCharacters(string) {
   }
 }
 
-// function printCharacters(string) {
-//   Array.from(string).forEach((char, index) => {
-//     console.log(`${index}: ${char}`)
-//   })
-// }
-
 printCharacters("hello");
+
+//You can make this function more space and time efficient by simply looping over the string without creating a new array to store your lines of strings.
+//It's considered best practice to avoid using the var keyword unless absolutely necessary, to avoid scope errors later in your work. Use const or let instead.
+//Your looping condition should be <, not <= because JS arrays and strings are zero indexed. In this scenario, you'll encounter an undefined variable on the last iteration.
+//Otherwise it's formatted well and readable, but can be improved.
