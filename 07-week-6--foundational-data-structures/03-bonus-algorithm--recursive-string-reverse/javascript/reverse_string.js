151496305f29b359d8e7ee4f87586c2fb5500df1
Cody <codybarker.or@gmail.com>
2024-10-24 11:43:02 -0700

    add recursive solution for reversing a string

--- 07-week-6--foundational-data-structures/03-bonus-algorithm--recursive-string-reverse/javascript/reverse_string.js
+++ 07-week-6--foundational-data-structures/03-bonus-algorithm--recursive-string-reverse/javascript/reverse_string.js
@@ -1,16 +1,20 @@
 function reverseString(str) {
-  // type your code here
+  if (str.length === 0) {
+    return "";
+  }
+
+  return str.slice(-1) + reverseString(str.slice(0, -1));
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
-  console.log("=>", reverseString('ih'));
+  console.log("=>", reverseString("ih"));
 
   console.log("");
 
   console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString('catbaby'));
+  console.log("=>", reverseString("catbaby"));
 }
 
 module.exports = reverseString;
