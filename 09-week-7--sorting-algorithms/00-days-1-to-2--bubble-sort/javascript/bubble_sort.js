74d91aba2040c662a91a0b99f21d34a8dfaf61ee
Cody <codybarker.or@gmail.com>
2024-10-26 10:42:57 -0700

    add bubble sort solution

--- 09-week-7--sorting-algorithms/00-days-1-to-2--bubble-sort/javascript/bubble_sort.js
+++ 09-week-7--sorting-algorithms/00-days-1-to-2--bubble-sort/javascript/bubble_sort.js
@@ -1,5 +1,22 @@
 function bubbleSort(arr) {
-  // type your code here
+  function swap(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        count++;
+      }
+    }
+    return count;
+  }
+
+  let swaps = swap(arr);
+
+  while (swaps > 0) {
+    swaps = swap(arr);
+  }
+
+  return arr;
 }
 
 if (require.main === module) {
