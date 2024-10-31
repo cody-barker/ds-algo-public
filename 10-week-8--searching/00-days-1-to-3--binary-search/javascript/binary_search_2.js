eb64a994fc5b7a58f0be3251f2eb2317a9a47d3c
Cody <codybarker.or@gmail.com>
2024-10-31 11:59:15 -0700

    add queue and dequeue solutions

--- 10-week-8--searching/00-days-1-to-3--binary-search/javascript/binary_search_2.js
+++ 10-week-8--searching/00-days-1-to-3--binary-search/javascript/binary_search_2.js
@@ -1,31 +1,52 @@
+// function binarySearchIndex(arr, target) {
+//   let left = 0;
+//   let right = arr.length - 1;
+
+//   while (left <= right) {
+//     debugger;
+//     // Calculate middle index
+//     // Using (left + right) >>> 1 avoids integer overflow
+//     // compared to Math.floor((left + right) / 2)
+//     const mid = Math.floor((left + right) / 2);
+
+//     // If target is found at mid, return it
+//     if (arr[mid] === target) {
+//       return mid;
+//     }
+
+//     // If target is greater, ignore left half
+//     if (arr[mid] < target) {
+//       left = mid + 1;
+//     }
+//     // If target is smaller, ignore right half
+//     else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   // Target not found
+//   return -1;
+// }
+
 function binarySearchIndex(arr, target) {
   let left = 0;
   let right = arr.length - 1;
 
   while (left <= right) {
-    debugger;
-    // Calculate middle index
-    // Using (left + right) >>> 1 avoids integer overflow
-    // compared to Math.floor((left + right) / 2)
-    const mid = Math.floor((left + right) / 2);
+    let mid = Math.floor((left + right) / 2);
 
-    // If target is found at mid, return it
     if (arr[mid] === target) {
       return mid;
     }
 
-    // If target is greater, ignore left half
     if (arr[mid] < target) {
       left = mid + 1;
-    }
-    // If target is smaller, ignore right half
-    else {
+    } else {
       right = mid - 1;
     }
   }
 
-  // Target not found
   return -1;
 }
 
-binarySearchIndex([1, 2, 3, 4, 5, 6], 2);
+console.log(binarySearchIndex([1, 2, 3, 4, 5, 6], 7));
