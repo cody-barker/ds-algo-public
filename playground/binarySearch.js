c6230839f05f71c95aec35323e86dd0f37647b4a
Cody Barker <codybarker.or@gmail.com>
2024-11-14 10:19:18 -0800

    add solution

--- playground/binarySearch.js
+++ playground/binarySearch.js
@@ -1,14 +1,22 @@
 function binarySearch(array, target) {
-    //variables for start, mid, and end
-    //check if mid === target, return target
-    //if mid < target, start = mid + 1
-    //if mid > target, end ==
+    let start = 0;
+    let end = array.length - 1;
 
+    while (start <= end) {
+        const mid = Math.floor((start + end) /2);
 
-    /*
-    const array = [1,2,3,4,5,7,10]
-    target = 7
-    start = array[0]
-    end = array
-    */
+        if (array[mid] === target) {
+            return mid
+        } else if (array[mid] < target) {
+            start = mid + 1;
+        } else if (array[mid] > target) {
+            end = mid - 1;
+        }
+    }
+
+    return -1
 }
+
+const array = [1,2,5,7,9,11,13,15];
+
+console.log(binarySearch(array, 13))
