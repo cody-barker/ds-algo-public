ba71df15c7b56307f26a29ca0cd275767826cb6f
Cody Barker <codybarker.or@gmail.com>
2024-11-14 10:06:24 -0800

    add binarySearch.js

---
+++ playground/binarySearch.js
@@ -0,0 +1,14 @@
+function binarySearch(array, target) {
+    //variables for start, mid, and end
+    //check if mid === target, return target
+    //if mid < target, start = mid + 1
+    //if mid > target, end ==
+
+
+    /*
+    const array = [1,2,3,4,5,7,10]
+    target = 7
+    start = array[0]
+    end = array
+    */
+}
