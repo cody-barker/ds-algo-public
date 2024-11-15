aff7429f7fcd81b07cb47c8e29984694e63f3964
Cody <codybarker.or@gmail.com>
2024-11-15 10:36:50 -0800

    add solution

--- playground/Sliding Window/minSubArrayLen.js
+++ playground/Sliding Window/minSubArrayLen.js
@@ -1,21 +1,43 @@
-function minSubArrayLen(target, nums) {
-  if (!nums.length) return 0;
+// function minSubArrayLen(target, nums) {
+//   if (!nums.length) return 0;
+
+//   let left = 0;
+//   let sum = 0;
+//   let minLength = Infinity;
+
+//   for (let right = 0; right < nums.length; right++) {
+//     sum += nums[right];
+    
+//     while (sum >= target) {
+//       minLength = Math.min(minLength, right - left + 1);
+//       sum -= nums[left];
+//       left++;
+//     }
+//   }
+
+//   return minLength === Infinity ? 0 : minLength;
+// }
+
+// console.log(minSubArrayLen(5, [1,6,4,5]))
+
+function minSubArrayLen(target, arr) {
+  if (!arr.length) return 0
 
   let left = 0;
   let sum = 0;
   let minLength = Infinity;
 
-  for (let right = 0; right < nums.length; right++) {
-    sum += nums[right];
-    
+  for (let right = 0; right < arr.length; right++) {
+    sum += arr[right];
+
     while (sum >= target) {
       minLength = Math.min(minLength, right - left + 1);
-      sum -= nums[left];
-      left++;
+      sum -= arr[left];
+      left++
     }
   }
 
   return minLength === Infinity ? 0 : minLength;
 }
 
-console.log(minSubArrayLen(5, [1,6,4,5]))
+console.log(minSubArrayLen(0, [1,6,4,5]))
